781eddfee5b15463f694c9ebbd00673d4b92c6be
Pepeoawvh <jpp.valdes@gmail.com>
2026-05-11 12:36:37 -0400

    Ejercicio menu ifelsewhiletryexcept

--- Menus.py
+++ Menus.py
@@ -1,34 +1,42 @@
 # El programa debe tener un menú de opciones de donde se pueda realizar el pago del cupo de la tarjeta de crédito, como también simular nuevas compras, y estas una vez sumadas se resten al cupo disponible. 
 # Las opciones disponibles deben estar construidas de la siguiente forma:
-saldo=100000
+cupo_disponible=0
+compra=0
+deuda=100000
+
 while True:
-
-    print("1. Pago tarjeta de credito")
-    print("2. Simulacion de compra")
-    print("3. Salir")
-    op=int(input("ingrese su opcion: "))
-
-    if op==1:
-        print("Pagando... ")
-        monto_pagar= int(input("Ingrese monto a pagar: "))
-        if monto_pagar>= 0:
-            if monto_pagar<=saldo:
-                saldo= saldo-monto_pagar
-                print(f"El saldo es {saldo}")
-    elif op ==2:
-        print("Cmprando")
-    elif op==3:
-        print("Saliendo")
-        break
-    else:
-        print("Elija una opcion valida")
+    try:
+        print("***************************")
+        print("1. Pago tarjeta de credito")
+        print("2. Simulacion de compra")
+        print("3. Salir")
+        op=int(input("ingrese su opcion: "))
 # 1.	Pago de Tarjeta de Crédito:
 # a.	El usuario comienza con una deuda de $100.000
-# b.	El usuario puede ingresar un monto para realizar un pago en la tarje-ta de crédito.
+# b.	El usuario puede ingresar un monto para realizar un pago en la tarjeta de crédito.
 # c.	Se debe verificar que el monto ingresado sea mayor o igual a cero.
 # d.	Se debe verificar que el monto a pagar no exceda el saldo actual de la tarjeta.
 # e.	Al pagar el sistema debe descontar de la deuda total
 # f.	Si las verificaciones son exitosas, se realiza el pago y se actualiza el saldo de la tarjeta.
+        if op==1:
+            while True:
+                try:
+                    print("Pagando... ")
+                    print(f"Ud mantiene una deuda de {deuda}")
+                    monto_pagar= int(input("Ingrese monto a pagar: "))
+                    if monto_pagar > 0 and monto_pagar<= deuda:
+                        if monto_pagar<=deuda:
+                            deuda= deuda-monto_pagar
+                            cupo_disponible= cupo_disponible+monto_pagar
+                            print(f"Ud pagó {monto_pagar}")
+                            print(f"Su cupo actual es {cupo_disponible}")
+                            break
+                    elif monto_pagar<0:
+                        print(f"Ingrese una cantidad valida")
+                    elif monto_pagar > deuda:
+                        print(f"No puede abonar un monto mayor a su cupo")
+                except:
+                    print("Debe ingresar solo numeros")
 # 2.	Simulación de Compras:
 # a.	El usuario puede simular realizar un número ilimitado de compras.
 # b.	Para cada compra, se solicita al usuario ingresar el monto de la compra. El programa suma los montos de cada compra. 
@@ -36,14 +44,38 @@ while True:
 # d.	Se realiza la compra y se actualiza el saldo de la tarjeta para cada iteración del bucle for.
 # 3.	Salir:
 # a.	Al seleccionar esta opción, el programa debe cerrarse o finalizar.
+        elif op ==2:
+            while True:
+                try:
+                    print("Comprando...")
+                    print(f"Recuerde: su cupo actual es {cupo_disponible}")
+                    monto_comprar= int(input("Ingrese monto a de su compra: "))
+                    if monto_comprar >= 0 and monto_comprar <= cupo_disponible:
+                        cupo_disponible= cupo_disponible-monto_comprar
+                        deuda = deuda+monto_comprar
+                        print(f"Compra realizada por {monto_comprar}")
+                        print(f"Su cupo disponible es {cupo_disponible}")
+                        break
+                    elif monto_comprar > cupo_disponible: 
+                        print(f"No puede comprar más que el cupo")
+                        print(f"Su cupo actual es {cupo_disponible}")   
+                        break            
+                    elif monto_comprar < 0:
+                        print(f"Ingrese una cantidad valida")
+                except:
+                    print("Debe ingresar solo numeros")
+                    
+        elif op==3:
+            print("Saliendo")
+            break
+        else:
+            print("Elija una opcion valida")       
+    except:
+        print("Debe ingresar solo numeros")
 
 # A considerar:
-# 1.	Manejo de Errores:Hey, Cortana, open. Hey, Cortana. Hey, Cortana. 
-# a.	Se utilizan bloques try y except para manejar posibles errores al in-gresar datos, validar valores no numéricos y errores inesperados. 
+# 1.	Manejo de Errores
+# a.	Se utilizan bloques try y except para manejar posibles errores al in-gresar datos, validar valores no numéricos y errores   inesperados. 
 # b.	Se debe programar mensajes de error específicos para guiar al usuario sobre posibles problemas.
 
 
-# Instrucciones para el envío de la actividad
-
-# El representante del grupo deberá comprimir los programas y enviar al docente a través de Mensajes de AVA, utilizando el siguiente formato para el nombre del ar-chivo:
-# NombreApellido.RAR
